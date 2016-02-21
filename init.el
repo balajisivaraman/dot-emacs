4e34f0f5dc8b062ac63baf47068cceca4872d7f1
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-02-21 20:56:50 +0530

    Require use-package and bind-key during emacs init

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;; Benchmarking Emacs Startup
 (defconst emacs-start-time (current-time))
 
+(defvar balaji/site-lisp-dir (expand-file-name "site-lisp" user-emacs-directory))
+(add-to-list 'load-path balaji/site-lisp-dir)
+(dolist (site-lisp-subdir-path (directory-files balaji/site-lisp-dir t "[^\s.]$"))
+  (add-to-list 'load-path site-lisp-subdir-path))
+(require 'use-package)
+(require 'bind-key)
+
 (when (window-system)
   (let ((elapsed-time (float-time (time-subtract (current-time)
 						 emacs-start-time))))
