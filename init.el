64a65004906bdaa59ef791e7a0b2f3364c3707dc
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-02-21 23:27:08 +0530

    Add some sane defaults for emacs

--- init.el
+++ init.el
@@ -29,6 +29,54 @@
 ;; We're going to use use-package for that.
 (setq package-enable-at-startup nil)
 
+;;; Better Defaults
+;; Hide the menu bar, tool bar and scroll bar
+(menu-bar-mode -1)
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;; Don't display the default start up message
+;; If we're loading from a saved desktop, this setting becomes redundant.
+(setq inhibit-startup-message t)
+;; Highlight the current line on all buffers
+(global-hl-line-mode t)
+;; Who ever wants to fully type out YES and NO
+(fset 'yes-or-no-p 'y-or-n-p)
+ ;; This will help us open up buffers without confirmation.
+(setq confirm-nonexistent-file-or-buffer nil)
+;; Setting word wrap mode as default
+(global-visual-line-mode 1)
+(diminish 'visual-line-mode)
+;; Highlight matching parens
+(show-paren-mode t)
+;; Show column number in bar
+(column-number-mode t)
+;; Typed text will replace the selection as in most modern editors
+(delete-selection-mode t)
+;; Display line numbers to the left of the buffer
+(global-linum-mode t)
+;; When I grew up, sentences always ended with a single space.
+(setq sentence-end-double-space nil)
+;; Prettify Symbols Mode in Emacs > 24.4 is awesome.
+(setq-default prettify-symbols-mode t)
+(global-prettify-symbols-mode t)
+;; Electric Pair Mode
+(electric-pair-mode t)
+
+(setq x-select-enable-clipboard t
+      save-interprogram-paste-before-kill t
+      mouse-yank-at-point t
+      visible-bell t
+      load-prefer-newer t)
+
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(use-package iso-transl)
+
+;; Automatically save buffers before launching M-x compile and friends,
+;; instead of asking you if you want to save.
+(setq compilation-ask-about-save nil)
+
 (if (balaji/network-connection-available-p)
     (package-refresh-contents))
 
