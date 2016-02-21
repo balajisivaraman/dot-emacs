72b4e913aa36f4bccccafcebe3b08466640dc284
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-02-21 21:14:23 +0530

    Add s.el and dash.el libraries and enable them

--- init.el
+++ init.el
@@ -1,12 +1,16 @@
 ;; Benchmarking Emacs Startup
 (defconst emacs-start-time (current-time))
 
+;;; Configure Use-Package
 (defvar balaji/site-lisp-dir (expand-file-name "site-lisp" user-emacs-directory))
 (add-to-list 'load-path balaji/site-lisp-dir)
-(dolist (site-lisp-subdir-path (directory-files balaji/site-lisp-dir t "[^\s.]$"))
-  (add-to-list 'load-path site-lisp-subdir-path))
+(add-to-list 'load-path (expand-file-name "use-package" balaji/site-lisp-dir))
 (require 'use-package)
 (require 'bind-key)
+
+;;; Configure Libraries
+(use-package s    :defer t :load-path "lib/s-el")
+(use-package dash :defer t :load-path "lib/dash-el")
 
 (when (window-system)
   (let ((elapsed-time (float-time (time-subtract (current-time)
