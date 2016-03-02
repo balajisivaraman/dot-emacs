836dddf0f80a0eec25f22cfe3abf059eb3e995c3
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-03-03 01:23:20 +0530

    Cleanup whitespace in init.el

--- init.el
+++ init.el
@@ -49,12 +49,12 @@
 
 (when (window-system)
   (let ((elapsed-time (float-time (time-subtract (current-time)
-						 emacs-start-time))))
+                         emacs-start-time))))
     (message "Loading %s...done (%.3fs)" load-file-name elapsed-time))
 
   (add-hook 'after-init-hook
-	    `(lambda ()
-	       (let ((elapsed-time (float-time (time-subtract (current-time)
-							      emacs-start-time))))
-		 (message "Loading %s...done (%.3fs)" ,load-file-name elapsed-time)))
-	    t))
+        `(lambda ()
+           (let ((elapsed-time (float-time (time-subtract (current-time)
+                                  emacs-start-time))))
+         (message "Loading %s...done (%.3fs)" ,load-file-name elapsed-time)))
+        t))
