4fd8539c31f632e7f5404aeeba49c176dbf55711
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-03-17 21:36:51 +0530

    Set racer rust path from rustup

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -62,7 +62,7 @@
   :config
   (setq
    racer-cmd "~/.cargo/bin/racer"
-   racer-rust-src-path "~/Projects/rust/rust-lang/src"))
+   racer-rust-src-path "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"))
 
 (use-package flycheck-rust
   :commands flycheck-rust-setup)
