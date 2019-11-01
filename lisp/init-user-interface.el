fde91353cf6fdee8dfb7b5977e0ce27dee4cfd3a
Balaji Sivaraman <balaji@balajisivaraman.com>
2019-11-01 20:48:05 +0530

    ui: switch to srcery theme

--- lisp/init-user-interface.el
+++ lisp/init-user-interface.el
@@ -19,13 +19,13 @@
 
 ;;; Commentary:
 
-;; Loads Dracula Theme, removes extraneous user-interface elements and makes Emacs more usable.
+;; Loads Srcery Theme, removes extraneous user-interface elements and makes Emacs more usable.
 
 ;;; Code:
 
-(use-package dracula-theme
+(use-package srcery-theme
   :init
-  (load-theme 'dracula t))
+  (load-theme 'srcery t))
 
 (use-package nyan-mode
   :init (nyan-mode))
